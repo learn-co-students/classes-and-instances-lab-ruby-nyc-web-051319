c010ed72a5063483f2101366ad60b14dec56790f
Elma Rahman <elmarahman635@gmail.com>
2019-05-14 18:37:23 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+
+
+end
+
+fido = Dog.new
+snoopy = Dog.new
+ lassie = Dog.new
+
+ #you wanna assign it variable name = _.new
+ #and you wanna define it outside the method so that
+ #it's accessible for other scopes
+#& and capitalize first letter of class!!! dont forget
